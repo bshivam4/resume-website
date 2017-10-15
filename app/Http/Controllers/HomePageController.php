3fbc777e3c0667c3303945de354bce8938d1f146
Shivam Bansal <bshivam4@gmail.com>
2017-10-15 15:48:27 +0530

    Education Model added

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\BasicProfile;
+use App\Education;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
     public function index(){
         $basic_profile=BasicProfile::first();
+        $educations=Education::get();
         return view('home',compact('basic_profile'));
     }
 }
