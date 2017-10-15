e04d7cd5c780f31187f676f74ac34086d82ab621
Shivam Bansal <bshivam4@gmail.com>
2017-10-15 13:39:24 +0530

    basic profile attributes migrations

---
+++ database/migrations/2017_10_15_080633_create_basic_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasicProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('basic_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('designation');
+            $table->string('facebook_url');
+            $table->string('linkedin_url');
+            $table->string('googleplus_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('basic_profiles');
+    }
+}
