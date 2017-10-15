30ff35bfe99884c523857a4325b3f5938d4185fd
Shivam Bansal <bshivam4@gmail.com>
2017-10-15 16:26:10 +0530

    Added experiences migrations

---
+++ database/migrations/2017_10_15_105401_create_experiences_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('designation');
+            $table->string('organisation');
+            $table->string('from');
+            $table->string('to');
+            $table->mediumText('description');
+            $table->mediumText('technology');
+            $table->integer('serial');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
