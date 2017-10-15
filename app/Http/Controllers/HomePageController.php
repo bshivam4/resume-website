30f8ef9906e1817266e722b66c3b42d521e7f809
Shivam Bansal <bshivam4@gmail.com>
2017-10-15 16:27:26 +0530

    Added experiences model

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\BasicProfile;
 use App\Education;
+use App\Experience;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
@@ -11,6 +12,7 @@ class HomePageController extends Controller
     public function index(){
         $basic_profile=BasicProfile::first();
         $educations=Education::get();
+        $experiences=Experience::get();
         return view('home',compact('basic_profile','educations'));
     }
 }
