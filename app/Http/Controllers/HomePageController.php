0a42b74bb44ff82611103df4d21c18c7f289504d
Shivam Bansal <bshivam4@gmail.com>
2017-10-16 15:53:11 +0530

    fetched skills from DB

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\BasicProfile;
 use App\Education;
 use App\Experience;
+use App\Skill;
 use App\Specialization;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomePageController extends Controller
 {
@@ -15,6 +17,20 @@ class HomePageController extends Controller
         $educations=Education::get();
         $experiences=Experience::orderBy('serial','desc')->get();
         $specializations=Specialization::orderBy('serial')->get();
-        return view('home',compact('basic_profile','educations','experiences','specializations'));
+        $skills_titles=Skill::select('title')->distinct()->get();
+        $i=0;
+        $skills=[];
+        foreach ($skills_titles as $skills_title){
+            $skills[$i]['title']=$skills_title->title;
+
+            $sub_skills=Skill::where('title',$skills_title->title)->get();
+            $j=0;
+            foreach ($sub_skills as $sub_skill){
+                $skills[$i]['subtitle'][$j++]=$sub_skill->sub_title;
+            }
+            $i++;
+        }
+
+        return view('home',compact('basic_profile','educations','experiences','specializations','skills'));
     }
 }
